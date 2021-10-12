b74f8180db04060dd31a8d34d53dccc914343cc3
Sokun Chorn <chornsokun@gmail.com>
2021-10-12 21:24:06 +1100

    replace slstatus with dwmblocks

--- suckless.sh
+++ suckless.sh
@@ -4,12 +4,15 @@ suckless=(
     "dwm-6.2"
     "dmenu-5.0"
     "st-0.8.2"
-    "slstatus"
+    "dwmblocks"
 )
 for app in "${suckless[@]}"
 do
-    pushd ./core/${app}
-    rm ./core/${app}/config.h
+    pkg=./core/${app}
+    pushd $pkg
+    if test -f "config.def.h"; then
+        rm config.h 2> /dev/null
+    fi
     make clean install
     popd
 done
